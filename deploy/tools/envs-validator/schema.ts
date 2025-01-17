a9b81feedce9b42de175f6dc8625eeb0a0e884af
syc <yonnk2021@163.com>
2025-01-17 12:02:16 +0800

    fix schema test error(#6) decline .json()

--- deploy/tools/envs-validator/schema.ts
+++ deploy/tools/envs-validator/schema.ts
@@ -353,7 +353,6 @@ const schema = yup
     //     f. customer color
     NEXT_PUBLIC_CUSTOM_COLOR: yup
       .object()
-      .json()
       .shape({
         brightThemeColor: yup
           .string()
